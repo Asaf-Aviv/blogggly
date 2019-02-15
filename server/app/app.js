a5f587b66d4f0d5cb63d2e959eea8a799e57edf0
Asaf Aviv <asafaviv89@gmail.com>
2019-02-15 12:30:57 +0200

    removed passport

--- server/app/app.js
+++ server/app/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-const passport = require('passport');
-const { ApolloServer, gql } = require('apollo-server-express');
-const User = require('../models/User');
-const Post = require('../models/Post');
+const { ApolloServer } = require('apollo-server-express');
+const isAuth = require('../middleware/isAuth');
+const typeDefs = require('../graphql/schema');
+const resolvers = require('../graphql/resolvers');
 
 const productionMode = process.env.NODE_ENV === 'production';
 
@@ -14,66 +14,12 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(isAuth);
 
-const typeDefs = gql`
-  type Query {
-    users: [User!]!
-    user(id: ID!): User
-    posts: [Post!]!
-    post(id: ID!): Post
-  }
-
-  type Mutation {
-    login(user: UserInput!): User
-    createUser(user: UserInput!): User
-    createPost(post: PostInput!): Post
-  }
-
-  type User {
-    _id: ID!
-    username: String!
-    email: String!
-    password: String
-    posts: [Post!]!
-  }
-
-  type Post {
-    _id: ID!
-    author: User!
-    title: String!
-    body: String!
-    createdAt: String!
-    updatedAt: String!
-  }
-
-  input PostInput {
-    author: ID!
-    title: String!
-    body: String!
-  }
-
-  input UserInput {
-    email: String!
-    password: String!
-    username: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    user: (parent, args) => User.findById(args.id),
-    users: () => User.find(),
-    post: async (parent, args) => Post.findById(args.id).populate('author'),
-    posts: () => Post.find(),
-  },
-  Mutation: {
-    login: (parent, args) => User.login(args.user),
-    createUser: (parent, args) => User.createUser(args.user),
-    createPost: (parent, args) => Post.createPost(args.post),
-  },
-};
+app.use((req, res, next) => {
+  console.log(req.isAuth);
+  next();
+});
 
 const apolloServer = new ApolloServer({
   typeDefs,
@@ -83,7 +29,11 @@ const apolloServer = new ApolloServer({
 apolloServer.applyMiddleware({ app });
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, `../../client/${productionMode ? 'dist' : 'public'}`, 'index.html'));
+  res.sendFile(
+    path.join(
+      __dirname, `../../client/${productionMode ? 'dist' : 'public'}`, 'index.html',
+    ),
+  );
 });
 
 module.exports = app;
