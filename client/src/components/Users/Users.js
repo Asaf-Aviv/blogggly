6eb5022bce19dda582d52ca09b714e12c96e1617
Asaf Aviv <asafaviv89@gmail.com>
2019-04-25 14:50:37 +0300

    Users search page

--- client/src/components/Users/Users.js
+++ client/src/components/Users/Users.js
@@ -1,30 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Query } from 'react-apollo';
 import queries from '../../graphql/queries';
 import Container from '../Container';
+import UserSearchBar from '../UserSearchBar';
+import UserCard from '../UserCard';
 
-const Users = () => (
-  <div className="users">
-    <Container>
-      <Query query={queries.ALL_USERS}>
-        {({ loading, error, data }) => {
-          if (loading) return <h1>loading</h1>;
-          if (error) return <h1>Error</h1>;
+import './Users.sass';
+import { UserContext } from '../../context';
 
-          return (
-            <div className="users">
-              {data.users.map(({ username, avatar }) => (
-                <div key={username}>
-                  <h1>{username}</h1>
-                  <img height="60" src={avatar} alt={username} />
-                </div>
-              ))}
-            </div>
-          );
-        }}
-      </Query>
-    </Container>
-  </div>
-);
+const Users = () => {
+  const [userQuery, setUserQuery] = useState('');
+
+  const { loggedUser, isLogged } = useContext(UserContext);
+
+  useEffect(() => {
+    console.log('setting query', userQuery);
+  }, [userQuery]);
+
+  return (
+    <main className="users__main">
+      <Container>
+        <UserSearchBar setUserQuery={setUserQuery} />
+        {userQuery && (
+          <Query
+            query={queries.SEARCH_USERS}
+            variables={{ userQuery }}
+          >
+            {({ loading, error, data: { users } = {} }) => {
+              if (loading) return <h1>loading</h1>;
+              if (error) return <h1>Error</h1>;
+
+              if (!users.length) {
+                return <h3 className="users__not-found">No users found.</h3>;
+              }
+
+              return (
+                <ul className="users__list">
+                  {users.map(user => (
+                    <li key={user._id}>
+                      <UserCard
+                        user={user}
+                        following={isLogged && loggedUser.following.includes(user._id)}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              );
+            }}
+          </Query>
+        )}
+      </Container>
+    </main>
+  );
+};
 
 export default Users;
