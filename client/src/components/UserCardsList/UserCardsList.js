9e2b1de63f056f47bff3654cecc6e373917a8fe9
Asaf Aviv <asafaviv89@gmail.com>
2019-04-28 19:44:49 +0300

    UserCardsList

---
+++ client/src/components/UserCardsList/UserCardsList.js
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react';
+import { arrayOf, string } from 'prop-types';
+import { Query } from 'react-apollo';
+import queries from '../../graphql/queries';
+import utils from '../../utils';
+import UserCard from '../UserCard';
+import { UserContext } from '../../context';
+
+const UserCardsList = ({ userIds }) => {
+  const { loggedUser } = useContext(UserContext);
+
+  return (
+    <Query
+      query={queries.GET_USERS_BY_IDS}
+      onError={utils.UIErrorNotifier}
+      variables={{ userIds }}
+    >
+      {({ loading, data: { users } }) => {
+        if (loading) return null;
+
+        return users.map(user => (
+          <UserCard
+            key={user._id}
+            user={user}
+            following={loggedUser.following.includes(user._id)}
+            isIncomingFriendRequest={loggedUser.incomingFriendRequests.includes(user._id)}
+            friendRequestPending={
+              loggedUser.incomingFriendRequests.includes(user._id)
+              || loggedUser.sentFriendRequests.includes(user._id)
+            }
+            isAFriend={loggedUser.friends.includes(user._id)}
+          />
+        ));
+      }}
+    </Query>
+  );
+};
+
+UserCardsList.propTypes = {
+  userIds: arrayOf(string).isRequired,
+};
+
+export default UserCardsList;
---
+++ client/src/components/UserCardsList/index.js
@@ -0,0 +1,3 @@
+import UserCardsList from './UserCardsList';
+
+export default UserCardsList;
