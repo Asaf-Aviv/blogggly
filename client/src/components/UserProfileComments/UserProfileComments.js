3474b6e4fb9c00c8f1450452e28cb0c6d5afd19e
Asaf Aviv <asafaviv89@gmail.com>
2019-04-29 15:33:45 +0300

    refactor

--- client/src/components/UserProfileComments/UserProfileComments.js
+++ client/src/components/UserProfileComments/UserProfileComments.js
@@ -1,73 +1,42 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { Query, Mutation } from 'react-apollo';
+import { Query } from 'react-apollo';
 import { Link } from 'react-router-dom';
 import {
   arrayOf, string, number, shape, func, bool,
 } from 'prop-types';
-import Alert from 'react-s-alert';
 import queries from '../../graphql/queries';
 import utils from '../../utils';
 import { UserShortSummaryPropTypes } from '../../propTypes';
 import Likes from '../Likes';
-import ConfirmationModal from '../ConfirmationModal';
+import DeleteCommentButton from '../DeleteCommentButton';
 import { UserContext } from '../../context';
 
 import './UserProfileComment.sass';
 
 const UserProfileComment = ({
   comment, comment: { post }, isDeleted, addToDeletionQueue,
-}) => {
-  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
-
-  return (
-    <li className={`user-profile-comment__container ${isDeleted ? 'deleted' : ''}`}>
-      <Link to={`/post/${post._id}`}>
-        <h5 className="user-profile-comment__post-title">
-          {`on ${post.author.username} post ${post.title}`}
-        </h5>
-      </Link>
-      <p className="user-profile-comment__comment-body">{comment.body}</p>
-      <div className="likes__wrapper">
-        <Likes
-          id={comment._id}
-          likesCount={comment.likesCount}
-          likes={comment.likes}
-        />
-        <Mutation
-          mutation={queries.DELETE_COMMENT}
-          variables={{ commentId: comment._id, postId: post._id }}
-          onCompleted={({ deleteComment: deletedCommentId }) => {
-            Alert.success('Comment deleted successfully.');
-            addToDeletionQueue(deletedCommentId);
-          }}
-        >
-          {deleteComment => (
-            <>
-              <button
-                type="button"
-                className="trash-btn"
-                onClick={() => setShowConfirmationModal(true)}
-              >
-                <i className="fas fa-trash" />
-              </button>
-              {showConfirmationModal && (
-                <ConfirmationModal
-                  confirmationQuestion="Are you sure you want to delete this comment?"
-                  onConfirm={async () => {
-                    await deleteComment();
-                    setShowConfirmationModal(false);
-                  }}
-                  onCancel={() => setShowConfirmationModal(false)}
-                  theme="danger"
-                />
-              )}
-            </>
-          )}
-        </Mutation>
-      </div>
-    </li>
-  );
-};
+}) => (
+  <li className={`user-profile-comment__container ${isDeleted ? 'deleted' : ''}`}>
+    <Link to={`/post/${post._id}`}>
+      <h4 className="user-profile-comment__post-title">
+        {`on ${post.author.username} post ${post.title}`}
+      </h4>
+    </Link>
+    <p className="user-profile-comment__comment-body">{comment.body}</p>
+    <div className="likes__wrapper">
+      <Likes
+        id={comment._id}
+        likesCount={comment.likesCount}
+        likes={comment.likes}
+      />
+      <DeleteCommentButton
+        commentId={comment._id}
+        postId={post._id}
+        onCompletedCb={addToDeletionQueue}
+      />
+    </div>
+  </li>
+);
 
 UserProfileComment.propTypes = {
   comment: shape({
