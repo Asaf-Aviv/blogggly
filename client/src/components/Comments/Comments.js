88303b5287923dcfea2636f24fcf1b0acd995f4b
Asaf Aviv <asafaviv89@gmail.com>
2019-05-11 02:59:16 +0300

    Added be the first one to comment when there is no comments

--- client/src/components/Comments/Comments.js
+++ client/src/components/Comments/Comments.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import { string, arrayOf } from 'prop-types';
+import { Query } from 'react-apollo';
 import Comment from '../Comment';
+import queries from '../../graphql/queries';
 import AddComment from '../AddComment';
-import { CommentPropTypes } from '../../propTypes';
 
 import './Comments.sass';
 
-const Comments = ({ comments, postId }) => (
+const Comments = ({ commentIds, postId }) => (
   <section className="comments">
     <header className="comments__header">
       <h2>Comments</h2>
     </header>
     <AddComment postId={postId} />
-    <ul className="comments__list">
-      {comments.map(comment => (
-        <Comment key={comment._id} comment={comment} postId={postId} />
-      ))}
-    </ul>
+    {commentIds.length > 0
+      ? (
+        <Query
+          query={queries.GET_COMMENTS_BY_IDS}
+          variables={{ commentIds, withAuthorInfo: true }}
+        >
+          {({ loading, data: { comments } = {} }) => {
+            if (loading) return null;
+
+            return (
+              <ul className="comments__list">
+                {comments.map(comment => (
+                  <Comment key={comment._id} comment={comment} postId={postId} />
+                ))}
+              </ul>
+            );
+          }}
+        </Query>
+      )
+      : <div className="comments__empty">Be the first one to comment</div>
+    }
   </section>
 );
 
 Comments.propTypes = {
   postId: string.isRequired,
-  comments: arrayOf(CommentPropTypes).isRequired,
+  commentIds: arrayOf(string).isRequired,
 };
 
 export default Comments;
