4022eb7431cf9d17b94c26d8dd8a32aac48074a8
Asaf Aviv <asafaviv89@gmail.com>
2019-05-11 03:00:14 +0300

    Added empty sentence

--- client/src/components/MessageBoard/MessageBoard.js
+++ client/src/components/MessageBoard/MessageBoard.js
@@ -2,19 +2,30 @@ import React from 'react';
 import { string, arrayOf } from 'prop-types';
 import { MessagePropTypes } from '../../propTypes';
 import Message from '../Message';
+import EmptySentence from '../EmptySentence';
 
-const MessageBoard = ({ messages, loggedUserId }) => (
-  <ul>
-    {messages.map(message => (
-      <Message
-        loggedUserId={loggedUserId}
-        key={message._id}
-        message={message}
-        fromOrTo={message.from._id === loggedUserId ? 'to' : 'from'}
-      />
-    ))}
-  </ul>
-);
+const MessageBoard = ({ messages, loggedUserId }) => {
+  if (!messages.length) {
+    return (
+      <div className="empty__container">
+        <EmptySentence />
+      </div>
+    );
+  }
+
+  return (
+    <ul>
+      {messages.map(message => (
+        <Message
+          loggedUserId={loggedUserId}
+          key={message._id}
+          message={message}
+          fromOrTo={message.from._id === loggedUserId ? 'to' : 'from'}
+        />
+      ))}
+    </ul>
+  );
+};
 
 MessageBoard.propTypes = {
   loggedUserId: string.isRequired,
