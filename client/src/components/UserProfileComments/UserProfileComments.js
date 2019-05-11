3b3fc9de797f3096346afb39d2f1d6951d2cf289
Asaf Aviv <asafaviv89@gmail.com>
2019-05-11 03:07:29 +0300

    extracted comment to UserProfileComment

--- client/src/components/UserProfileComments/UserProfileComments.js
+++ client/src/components/UserProfileComments/UserProfileComments.js
@@ -1,66 +1,19 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, {
+  useState, useContext, useEffect, useRef,
+} from 'react';
 import { Query } from 'react-apollo';
-import { Link } from 'react-router-dom';
-import {
-  arrayOf, string, number, shape, func, bool,
-} from 'prop-types';
+import { arrayOf, string } from 'prop-types';
 import queries from '../../graphql/queries';
 import utils from '../../utils';
-import { UserShortSummaryPropTypes } from '../../propTypes';
-import Likes from '../Likes';
-import DeleteCommentButton from '../DeleteCommentButton';
 import { UserContext } from '../../context';
-
-import './UserProfileComment.sass';
-
-const UserProfileComment = ({
-  comment, comment: { post }, isDeleted, addToDeletionQueue,
-}) => (
-  <li className={`user-profile-comment__container ${isDeleted ? 'deleted' : ''}`}>
-    <Link to={`/post/${post._id}`}>
-      <h4 className="user-profile-comment__post-title">
-        {`on ${post.author.username} post ${post.title}`}
-      </h4>
-    </Link>
-    <p className="user-profile-comment__comment-body">{comment.body}</p>
-    <div className="likes__wrapper">
-      <Likes
-        id={comment._id}
-        likesCount={comment.likesCount}
-        likes={comment.likes}
-      />
-      <DeleteCommentButton
-        commentId={comment._id}
-        postId={post._id}
-        onCompletedCb={addToDeletionQueue}
-      />
-    </div>
-  </li>
-);
-
-UserProfileComment.propTypes = {
-  comment: shape({
-    _id: string.isRequired,
-    body: string.isRequired,
-    createdAt: string.isRequired,
-    likes: arrayOf(string).isRequired,
-    likesCount: number.isRequired,
-    post: shape({
-      _id: string.isRequired,
-      title: string.isRequired,
-      author: UserShortSummaryPropTypes.isRequired,
-    }).isRequired,
-  }).isRequired,
-  isDeleted: bool.isRequired,
-  addToDeletionQueue: func.isRequired,
-};
-
-let cacheDeletedComments = [];
+import UserProfileComment from '../UserProfileComment/UserProfileComment';
 
 const UserProfileComments = ({ commentIds }) => {
   const [deletedCommentsIds, setDeletedCommentsIds] = useState([]);
 
   const { loggedUser, setLoggedUser } = useContext(UserContext);
+
+  const cacheDeletedComments = useRef([]);
 
   const addToDeletionQueue = (commentId) => {
     setDeletedCommentsIds([...deletedCommentsIds, commentId]);
@@ -68,13 +21,13 @@ const UserProfileComments = ({ commentIds }) => {
 
   useEffect(() => {
     if (deletedCommentsIds.length) {
-      cacheDeletedComments = [...deletedCommentsIds];
+      cacheDeletedComments.current = [...deletedCommentsIds];
     }
   }, [deletedCommentsIds]);
 
   useEffect(() => () => {
     const updatedComments = loggedUser.comments
-      .filter(commentId => !cacheDeletedComments.includes(commentId));
+      .filter(commentId => !cacheDeletedComments.current.includes(commentId));
 
     setLoggedUser({
       ...loggedUser,
@@ -82,7 +35,7 @@ const UserProfileComments = ({ commentIds }) => {
       likes: {
         ...loggedUser.likes,
         comments: loggedUser.likes.comments
-          .filter(commentId => !cacheDeletedComments.includes(commentId)),
+          .filter(commentId => !cacheDeletedComments.current.includes(commentId)),
       },
     });
   }, []);
@@ -114,7 +67,6 @@ const UserProfileComments = ({ commentIds }) => {
     </Query>
   );
 };
-
 
 UserProfileComments.propTypes = {
   commentIds: arrayOf(string).isRequired,
