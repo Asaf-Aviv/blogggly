aeee7c6375234dbb9e124660b3375f8ee1f6f5df
Asaf Aviv <asafaviv89@gmail.com>
2019-05-11 03:17:30 +0300

    Added friend requests actions

--- client/src/components/UserActionsPanel/UserActionsPanel.js
+++ client/src/components/UserActionsPanel/UserActionsPanel.js
@@ -1,22 +1,121 @@
 import React, { useState } from 'react';
-import { bool, string } from 'prop-types';
+import {
+  bool, string, oneOfType, node, arrayOf,
+} from 'prop-types';
 import FollowButton from '../FollowButton';
 import SendMessageModal from '../SendMessageModal';
-import ReportModal from '../ReportModal/ReportModal';
+import ReportModal from '../ReportModal';
+import FriendRequestActions from '../FriendRequestActions';
 
 import './UserActionsPanel.sass';
 
-const UserActionsPanel = ({ following, userId, username }) => {
+const PopUp = ({ children }) => (
+  <div className="pop-up">
+    {children}
+  </div>
+);
+
+PopUp.propTypes = {
+  children: oneOfType([node, arrayOf(node)]).isRequired,
+};
+
+const UserActionsPanel = ({
+  following, isAFriend, friendRequestPending, userId, username,
+  isIncomingFriendRequest,
+}) => {
   const [showMessageModal, setShowMessageModal] = useState(false);
   const [showReportModal, setShowReportModal] = useState(false);
+  const [showFriendRequestPopUp, setShowFriendRequestPopUp] = useState(false);
+
+  const setNextPopUpState = nextState => () => {
+    setShowFriendRequestPopUp(nextState || !showFriendRequestPopUp);
+  };
 
   return (
     <div className="actions-panel">
       <div className="action" onClick={() => setShowMessageModal(true)}>
-        <i className="action__icon far fa-envelope" />
+        <i className="action__icon fas fa-envelope" />
       </div>
+      {!isAFriend && !friendRequestPending && (
+        <FriendRequestActions
+          toSend
+          userId={userId}
+          render={sendFriendRequest => (
+            <div className="action" onClick={sendFriendRequest}>
+              <i className="action__icon fas fa-user-plus" />
+            </div>
+          )}
+        />
+      )}
+      {isAFriend && (
+        <div className="action">
+          <i className="action__icon fas fa-user-check" />
+        </div>
+      )}
+      {friendRequestPending && (
+        isIncomingFriendRequest
+          ? (
+            <div
+              className="action action--friend-request"
+              onClick={setNextPopUpState()}
+            >
+              <i className="action__icon fas fa-user-clock" />
+              {showFriendRequestPopUp && (
+                <PopUp>
+                  <FriendRequestActions
+                    incoming
+                    userId={userId}
+                    username={username}
+                    render={(declineFriendRequest, accpetFriendRequest) => (
+                      <div className="friend-request-actions">
+                        <button
+                          className="btn btn--sm btn--default"
+                          type="button"
+                          onClick={declineFriendRequest}
+                        >
+                        Decline
+                        </button>
+                        <button
+                          className="btn btn--sm btn--success"
+                          type="button"
+                          onClick={accpetFriendRequest}
+                        >
+                        Accpet
+                        </button>
+                      </div>
+                    )}
+                  />
+                </PopUp>
+              )}
+            </div>
+          )
+          : (
+            <div className="action" onClick={setNextPopUpState()}>
+              <i className="action__icon fas fa-user-clock" />
+              {showFriendRequestPopUp && (
+                <PopUp>
+                  <FriendRequestActions
+                    userId={userId}
+                    pending
+                    render={cancelFriendRequest => (
+                      <div className="friend-request-actions">
+                        <button
+                          className="btn btn--sm btn--default"
+                          type="button"
+                          onClick={cancelFriendRequest}
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    )}
+                  />
+                </PopUp>
+              )}
+            </div>
+          )
+      )}
       <div className="action" onClick={() => setShowReportModal(true)}>
-        <i className="action__icon far fa-flag" />
+        <i className="action__icon fas fa-flag" />
       </div>
       <FollowButton following={following} userId={userId} username={username} />
       {showMessageModal && (
@@ -39,6 +138,9 @@ const UserActionsPanel = ({ following, userId, username }) => {
 
 UserActionsPanel.propTypes = {
   following: bool.isRequired,
+  isIncomingFriendRequest: bool.isRequired,
+  friendRequestPending: bool.isRequired,
+  isAFriend: bool.isRequired,
   userId: string.isRequired,
   username: string.isRequired,
 };
