21590303e0b9093f2ff13884f26054fb7b3f8a43
Asaf Aviv <asafaviv89@gmail.com>
2019-05-11 03:01:28 +0300

    refactor

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Query, Mutation } from 'react-apollo';
+import { Query } from 'react-apollo';
 import { shape, string } from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import moment from 'moment';
@@ -13,24 +13,25 @@ import FollowButton from '../FollowButton';
 import utils from '../../utils';
 import { UserContext } from '../../context';
 import Tags from '../Tags/Tags';
+import DeletePostButton from '../DeletePostButton';
 
 import './Post.sass';
 
 const Post = ({ match: { params: { postId } } }) => {
-  const { loggedUser } = useContext(UserContext);
+  const { loggedUser, isLogged } = useContext(UserContext);
 
   return (
-    <Query
-      query={queries.POST}
-      variables={{ postId, withComments: true }}
-      onError={utils.UIErrorNotifier}
-    >
-      {({ loading, error, data: { post } = {} }) => {
-        if (loading) return null;
-        if (error) return <Redirect to="/" />;
+    <main className="post__main">
+      <Query
+        query={queries.POST}
+        variables={{ postId }}
+        onError={utils.UIErrorNotifier}
+      >
+        {({ loading, error, data: { post } = {} }) => {
+          if (loading) return null;
+          if (error) return <Redirect to="/" />;
 
-        return (
-          <main className="post__main">
+          return (
             <Container>
               <article className="post">
                 <header className="post__header">
@@ -41,44 +42,39 @@ const Post = ({ match: { params: { postId } } }) => {
                 </header>
                 <AuthorDetails {...post.author}>
                   {post.author._id !== (loggedUser && loggedUser._id) && (
-                    <FollowButton
-                      following={!!loggedUser && loggedUser.following.includes(post.author._id)}
-                      userId={post.author._id}
-                      username={post.author.username}
-                    />
+                  <FollowButton
+                    following={!!loggedUser && loggedUser.following.includes(post.author._id)}
+                    userId={post.author._id}
+                    username={post.author.username}
+                  />
                   )}
                   <span className="post__date">{moment(+post.createdAt).format('LL')}</span>
                 </AuthorDetails>
                 <span className="post__body" dangerouslySetInnerHTML={{ __html: post.body }} />
-                <Likes
-                  postId={postId}
-                  likesCount={post.likesCount}
-                  id={postId}
-                  likes={post.likes}
-                  isPost
-                />
-                {loggedUser && loggedUser._id === post.author._id && (
-                  <Mutation
-                    mutation={queries.DELETE_POST}
-                    variables={{ postId: post._id }}
-                  >
-                    {deletePost => (
-                      <button type="button" onClick={deletePost}>Delete</button>
-                    )}
-                  </Mutation>
-                )}
+                <div className="post__actions">
+                  <Likes
+                    postId={postId}
+                    likesCount={post.likesCount}
+                    id={postId}
+                    likes={post.likes}
+                    isPost
+                  />
+                  {isLogged && loggedUser._id === post.author._id && (
+                  <DeletePostButton postId={postId} />
+                  )}
+                </div>
               </article>
               <MoreFromAuthor
                 authorName={post.author.username}
                 authorId={post.author._id}
                 viewingPostId={postId}
               />
-              <Comments postId={post._id} comments={post.comments} />
+              <Comments postId={post._id} commentIds={post.comments} />
             </Container>
-          </main>
-        );
-      }}
-    </Query>
+          );
+        }}
+      </Query>
+    </main>
   );
 };
 
