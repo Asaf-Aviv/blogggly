6c323b1311006dc10e269a7185015cbf4daf3a3c
Asaf Aviv <asafaviv89@gmail.com>
2019-05-12 23:20:04 +0300

    added isAuthor to ShowcaseCard

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,34 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Query } from 'react-apollo';
 import { string, shape } from 'prop-types';
 import ShowcaseCard from '../ShowcaseCard';
 import Container from '../Container';
 import queries from '../../graphql/queries';
 import utils from '../../utils';
+import { UserContext } from '../../context';
 
 import './Posts.sass';
 
-const Posts = ({ match: { params: { tag } } }) => (
-  <Query
-    query={queries.GET_POSTS_BY_TAG}
-    variables={{ tag }}
-    onError={utils.UIErrorNotifier}
-  >
-    {({ loading, data: { posts } }) => {
-      if (loading) return null;
+const Posts = ({ match: { params: { tag } } }) => {
+  const { loggedUser, isLogged } = useContext(UserContext);
 
-      return (
-        <Container>
-          <div className="posts__container">
-            {posts.map(post => (
-              <ShowcaseCard key={post._id} post={post} />
-            ))}
-          </div>
-        </Container>
-      );
-    }}
-  </Query>
-);
+  return (
+    <Query
+      query={queries.GET_POSTS_BY_TAG}
+      variables={{ tag }}
+      onError={utils.UIErrorNotifier}
+    >
+      {({ loading, data: { posts } }) => {
+        if (loading) return null;
+
+        return (
+          <Container>
+            <div className="posts__container">
+              {posts.map(post => (
+                <ShowcaseCard
+                  key={post._id}
+                  post={post}
+                  isAuthor={isLogged && loggedUser._id === post.author._id}
+                />
+              ))}
+            </div>
+          </Container>
+        );
+      }}
+    </Query>
+  );
+};
 
 Posts.propTypes = {
   match: shape({
