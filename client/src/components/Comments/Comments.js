08f1ab426e61d2dfb49f6b40724f1f881e19e89c
Asaf Aviv <asafaviv89@gmail.com>
2019-05-19 10:57:02 +0300

    refactor

--- client/src/components/Comments/Comments.js
+++ client/src/components/Comments/Comments.js
@@ -1,45 +1,47 @@
-import React from 'react';
-import { string, arrayOf } from 'prop-types';
+import React, { useState } from 'react';
+import { string } from 'prop-types';
 import { Query } from 'react-apollo';
 import Comment from '../Comment';
 import queries from '../../graphql/queries';
 import AddComment from '../AddComment';
+import SortByPanel from '../SortByPanel';
 
 import './Comments.sass';
 
-const Comments = ({ commentIds, postId }) => (
-  <section className="comments">
-    <header className="comments__header">
-      <h2>Comments</h2>
-    </header>
-    <AddComment postId={postId} />
-    {commentIds.length > 0
-      ? (
-        <Query
-          query={queries.GET_COMMENTS_BY_IDS}
-          variables={{ commentIds, withAuthorInfo: true }}
-        >
-          {({ loading, data: { comments } = {} }) => {
-            if (loading) return null;
+const Comments = ({ postId }) => {
+  const [sortBy, setSortBy] = useState('DATE_DESC');
 
-            return (
+  return (
+    <section className="comments">
+      <header className="comments__header">
+        <h2>Comments</h2>
+      </header>
+      <AddComment postId={postId} sortBy={sortBy} />
+      <SortByPanel setSortBy={setSortBy} />
+      <Query
+        query={queries.POST_COMMENTS}
+        variables={{ postId, sortBy }}
+      >
+        {({ loading, data: { comments } }) => {
+          if (loading) return null;
+
+          return comments.length
+            ? (
               <ul className="comments__list">
                 {comments.map(comment => (
                   <Comment key={comment._id} comment={comment} postId={postId} />
                 ))}
               </ul>
-            );
-          }}
-        </Query>
-      )
-      : <div className="comments__empty">Be the first one to comment</div>
-    }
-  </section>
-);
+            )
+            : <div className="comments__empty">Be the first one to comment</div>;
+        }}
+      </Query>
+    </section>
+  );
+};
 
 Comments.propTypes = {
   postId: string.isRequired,
-  commentIds: arrayOf(string).isRequired,
 };
 
 export default Comments;
