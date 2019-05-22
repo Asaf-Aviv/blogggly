9bac3cf4e816bb068c36b5689c1fa4dcb9b9317b
Asaf Aviv <asafaviv89@gmail.com>
2019-05-22 11:22:40 +0300

    changed Subscription component for subsciprionHandler helper

--- client/src/components/Comments/Comments.js
+++ client/src/components/Comments/Comments.js
@@ -1,65 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { string } from 'prop-types';
-import { Query, Subscription } from 'react-apollo';
+import { Query, withApollo } from 'react-apollo';
 import orderBy from 'lodash.orderby';
 import Comment from '../Comment';
 import queries from '../../graphql/queries';
 import AddComment from '../AddComment';
 import SortByPanel from '../SortByPanel';
+import apolloClient from '../../ApolloClient';
+import { subscriptionHandler } from '../../graphql/helpers';
 
 import './Comments.sass';
 
 const Comments = ({ postId }) => {
   const [sortBy, setSortBy] = useState({ key: 'createdAt', order: 'desc' });
 
+  useEffect(() => {
+    const variables = { postId };
+
+    const cacheUpdateFn = (result, query) => {
+      const data = apolloClient.readQuery(query);
+      data.comments.push(result);
+      apolloClient.writeQuery({ ...query, data });
+    };
+
+    const newCommentsSubscription = subscriptionHandler(
+      { query: queries.POST_COMMENTS, variables },
+      { query: queries.NEW_POST_COMMENT, variables },
+      cacheUpdateFn,
+    );
+
+    return () => newCommentsSubscription.unsubscribe();
+  }, [postId]);
+
   return (
-    <>
-      <Subscription
-        subscription={queries.NEW_POST_COMMENT}
+    <section className="comments">
+      <header className="comments__header">
+        <h2>Comments</h2>
+      </header>
+      <AddComment postId={postId} />
+      <SortByPanel setSortBy={setSortBy} />
+      <Query
+        query={queries.POST_COMMENTS}
         variables={{ postId }}
-        onSubscriptionData={({ client, subscriptionData }) => {
-          const { newPostComment } = subscriptionData.data;
-
-          const query = {
-            query: queries.POST_COMMENTS,
-            variables: { postId },
-          };
-
-          const data = client.cache.readQuery(query);
-          data.comments.push(newPostComment);
-          client.cache.writeQuery({ ...query, data });
-        }}
       >
-        {() => (
-          <section className="comments">
-            <header className="comments__header">
-              <h2>Comments</h2>
-            </header>
-            <AddComment postId={postId} />
-            <SortByPanel setSortBy={setSortBy} />
-            <Query
-              query={queries.POST_COMMENTS}
-              variables={{ postId }}
-            >
-              {({ loading, data: { comments } }) => {
-                if (loading) return null;
-                if (!comments.length) return <div className="comments__empty">Be the first one to comment</div>;
+        {({ loading, data: { comments } }) => {
+          if (loading) return null;
+          if (!comments.length) return <div className="comments__empty">Be the first one to comment</div>;
 
-                return (
-                  <ul className="comments__list">
-                    {orderBy(comments, sortBy.key, sortBy.order)
-                      .map(comment => (
-                        <Comment key={comment._id} comment={comment} postId={postId} />
-                      ))
-                  }
-                  </ul>
-                );
-              }}
-            </Query>
-          </section>
-        )}
-      </Subscription>
-    </>
+          return (
+            <ul className="comments__list">
+              {orderBy(comments, sortBy.key, sortBy.order)
+                .map(comment => (
+                  <Comment key={comment._id} comment={comment} postId={postId} />
+                ))
+            }
+            </ul>
+          );
+        }}
+      </Query>
+    </section>
   );
 };
 
@@ -67,4 +66,4 @@ Comments.propTypes = {
   postId: string.isRequired,
 };
 
-export default Comments;
+export default withApollo(Comments);
