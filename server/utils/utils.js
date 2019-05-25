b361bb5b24aeccaecf39dfaa948352987d17c572
Asaf Aviv <asafaviv89@gmail.com>
2019-05-25 12:15:18 +0300

    fixed dataLoaders

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -20,9 +20,9 @@ exports.generateToken = (userId) => {
 };
 
 exports.createLoaders = () => ({
-  userLoader: new DataLoader(userIds => User.findUsersByIds(userIds)),
-  postLoader: new DataLoader(postIds => Post.findPostsByIds(postIds)),
-  commentLoader: new DataLoader(commentIds => Comment.findCommentsByIds(commentIds)),
+  userLoader: new DataLoader(async userIds => userIds.map(id => User.findById(id))),
+  postLoader: new DataLoader(async postIds => postIds.map(id => Post.findById(id))),
+  commentLoader: new DataLoader(async commentIds => commentIds.map(id => Comment.findById(id))),
 });
 
 const titles = [
@@ -44,8 +44,8 @@ exports.createFakeData = async () => {
       let userInput;
       if (i) {
         userInput = {
-          username: `testeroni_${i}`,
-          email: `testeroni_${i}@test.com`,
+          username: `test${i}`,
+          email: `test${i}@test.com`,
           password: '12345',
         };
       } else {
@@ -59,53 +59,53 @@ exports.createFakeData = async () => {
     }),
   );
 
-  await Promise.all(
-    [...Array(5)].map(async (_, i) => { // eslint-disable-line
-      const postInput = {
-        author: fakeUsers[i],
-        title: sample(titles),
-        body: `
-        Lorem ipsum dolor sit, amet consectetur 
-        adipisicing elit. Dolore laboriosam praesentium ab 
-        animi totam! Quas sapiente ipsa nobis porro unde dignissimos 
-        expedita incidunt doloribus corrupti quasi enim ullam voluptas ea 
-        que magni, nemo fugit culpa cum dolores deleniti sequi explicabo laborum.
+  // await Promise.all(
+  //   [...Array(5)].map(async (_, i) => { // eslint-disable-line
+  //     const postInput = {
+  //       author: fakeUsers[i],
+  //       title: sample(titles),
+  //       body: `
+  //       Lorem ipsum dolor sit, amet consectetur
+  //       adipisicing elit. Dolore laboriosam praesentium ab
+  //       animi totam! Quas sapiente ipsa nobis porro unde dignissimos
+  //       expedita incidunt doloribus corrupti quasi enim ullam voluptas ea
+  //       que magni, nemo fugit culpa cum dolores deleniti sequi explicabo laborum.
 
-        Loa nobis porro unde dignissimos 
-        expedita incidunt doloribus corrupti quasi enim ullam voluptas ea 
-        que magni, nemo fugit culpa cum dolores deleniti sequi explicabo laborum.
+  //       Loa nobis porro unde dignissimos
+  //       expedita incidunt doloribus corrupti quasi enim ullam voluptas ea
+  //       que magni, nemo fugit culpa cum dolores deleniti sequi explicabo laborum.
 
-        Lorem ipsum dolor sit, amet consectetur 
-        adipisicing elit. Dolore laboriosam praesentium ab 
-        animi totam!nde dignissimos 
-        expedita incidunt doloribus corrupti quasi enim ullam voluptas ea 
-        que magni, nemo fugit culpa cum dolores deleniti sequi explicabo laborum.
-      `,
-        tags: ['React', 'Programming'],
-      };
-      return Post.createPost(postInput);
-    }),
-  );
+  //       Lorem ipsum dolor sit, amet consectetur
+  //       adipisicing elit. Dolore laboriosam praesentium ab
+  //       animi totam!nde dignissimos
+  //       expedita incidunt doloribus corrupti quasi enim ullam voluptas ea
+  //       que magni, nemo fugit culpa cum dolores deleniti sequi explicabo laborum.
+  //     `,
+  //       tags: ['React', 'Programming'],
+  //     };
+  //     return Post.createPost(postInput);
+  //   }),
+  // );
 
-  const users = await User.find();
-  const posts = await Post.find();
+  // const users = await User.find();
+  // const posts = await Post.find();
   // eslint-disable
-  for (const post of posts) {// eslint-disable-line
-    for (const user of users) {// eslint-disable-line
-      await Comment.addComment({ // eslint-disable-line
-        author: user._id,
-        post: post._id,
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, cum.',
-      });
-    }
-  }
+  // for (const post of posts) {// eslint-disable-line
+  //   for (const user of users) {// eslint-disable-line
+  //     await Comment.addComment({ // eslint-disable-line
+  //       author: user._id,
+  //       post: post._id,
+  //       body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, cum.',
+  //     });
+  //   }
+  // }
 
-  for (const user1 of users) {// eslint-disable-line
-    await User.findByIdAndUpdate(user1._id, { $set: { 'info.bio': sample(titles) } });// eslint-disable-line
-    for (const user2 of users) {// eslint-disable-line
-      if (user1.username !== user2.username) {
-        await User.sendMessage(user1._id, user2._id, 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, tempora.');// eslint-disable-line
-      }
-    }
-  }
+  // for (const user1 of users) {// eslint-disable-line
+  //   await User.findByIdAndUpdate(user1._id, { $set: { 'info.bio': sample(titles) } });// eslint-disable-line
+  //   for (const user2 of users) {// eslint-disable-line
+  //     if (user1.username !== user2.username) {
+  //       await User.sendMessage(user1._id, user2._id, `from ${user2.username}`) // eslint-disable-line
+  //     }
+  //   }
+  // }
 };
