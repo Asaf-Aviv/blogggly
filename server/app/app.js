dd1b755f956809862142f6d09d68d4a0eb1b14bf
Asaf Aviv <asafaviv89@gmail.com>
2019-05-28 16:36:31 +0300

    added upload route

--- server/app/app.js
+++ server/app/app.js
@@ -2,22 +2,21 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
 const compression = require('compression');
 const { createServer } = require('http');
 const path = require('path');
-const { ApolloServer } = require('apollo-server-express');
+const { multerUploader } = require('../utils');
+const handleFileUpload = require('../middleware/handleFileUpload');
+const checkAuth = require('../middleware/checkAuth');
 const isAuth = require('../middleware/isAuth');
-const typeDefs = require('../graphql/schema');
-const resolvers = require('../graphql/resolvers');
-const { createLoaders } = require('../utils');
-const pubsub = require('../graphql/pubsub');
+const apolloServer = require('../apolloServer');
 
 const app = express();
 const httpServer = createServer(app);
 
-const devMode = process.env.NODE_ENV !== 'production';
 const PORT = process.env.NODE_ENV || 5000;
+const devMode = process.env.NODE_ENV !== 'production';
+
 
 if (devMode) {
   mongoose.set('debug', true);
@@ -26,57 +25,19 @@ if (devMode) {
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use('/public', express.static(path.join(__dirname, '../public')));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(compression());
 app.use(isAuth);
 
-const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers,
-  subscriptions: {
-    onConnect: (connectionParams) => {
-      console.log('*'.repeat(20));
-      console.log(connectionParams);
-      console.log('*'.repeat(20));
-      console.log('websocket connected');
-      console.log('*'.repeat(20));
+app.get('/ping', (req, res) => res.send('pong'));
 
-      let currentUserId = null;
 
-      if (connectionParams.Authorization) {
-        try {
-          const token = connectionParams.Authorization.replace('Bearer ', '');
-          const { userId } = jwt.verify(token, process.env.JWT_SECRET);
-          currentUserId = userId;
-        // eslint-disable-next-line no-empty
-        } catch (e) {}
-      }
-
-      return {
-        currentUserId,
-      };
-    },
-  },
-  context: ({ req, connection }) => {
-    if (connection) {
-      console.log('context', connection.context);
-      return {
-        ...connection.context,
-        ...createLoaders(),
-        pubsub,
-      };
-    }
-
-    return {
-      ...createLoaders(),
-      pubsub,
-      isAuth: req.isAuth,
-      userId: req.userId,
-    };
-  },
+app.post('/upload', checkAuth, (req, res) => {
+  multerUploader.single('avatar')(req, res, err => handleFileUpload(req, res, err));
 });
 
 apolloServer.applyMiddleware({ app });
