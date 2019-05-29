1ed4165c2b50a0d1f3d278832c7ad7fea424e831
Asaf Aviv <asafaviv89@gmail.com>
2019-05-29 18:28:51 +0300

    added lifecycle events for Redis

--- server/redisClient/index.js
+++ server/redisClient/index.js
@@ -5,8 +5,24 @@ bluebird.promisifyAll(redis);
 
 const redisClient = redis.createClient();
 
+redisClient.on('ready', () => {
+  console.log('Redis connection established');
+});
+
+redisClient.on('connect', () => {
+  console.log('Redis stream connected');
+});
+
+redisClient.on('reconnect', () => {
+  console.log('Reonnecing to Redis');
+});
+
 redisClient.on('error', (err) => {
   console.error(`Redis Error: ${err}`);
+});
+
+redisClient.on('end', () => {
+  console.error('Connection to Redis has been closed');
 });
 
 setInterval(() => {
