7edca5ed96c1b7265e009232c6b4c8c8a4a46bb1
Asaf Aviv <asafaviv89@gmail.com>
2019-05-29 17:55:25 +0300

    added delete button if the logged user is the author

--- client/src/components/Comment/Comment.js
+++ client/src/components/Comment/Comment.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { string } from 'prop-types';
+import { string, bool } from 'prop-types';
 import moment from 'moment';
 import AuthorDetails from '../AuthorDetails';
 import Likes from '../Likes/Likes';
 import { CommentPropTypes } from '../../propTypes';
+import DeleteCommentButton from '../DeleteCommentButton';
 
 import './Comment.sass';
 
-const Comment = ({ comment, postId }) => (
+const Comment = ({ comment, postId, isAuthor }) => (
   <li className="comment">
     <AuthorDetails {...comment.author}>
       <span className="comment__date">{moment(+comment.createdAt).startOf('seconds').fromNow()}</span>
@@ -19,12 +20,16 @@ const Comment = ({ comment, postId }) => (
       likes={comment.likes}
       postId={postId}
     />
+    {isAuthor && (
+      <DeleteCommentButton commentId={comment._id} postId={postId} />
+    )}
   </li>
 );
 
 Comment.propTypes = {
   postId: string.isRequired,
   comment: CommentPropTypes.isRequired,
+  isAuthor: bool.isRequired,
 };
 
 export default Comment;
