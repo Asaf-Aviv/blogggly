735f61ddb42dbafd0cfad6ec34a68d29730b9dc1
Asaf Aviv <asafaviv89@gmail.com>
2019-06-02 01:59:44 +0300

    NotificationItem

---
+++ client/src/components/NotificationItem/NotificationItem.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { func, shape, string } from 'prop-types';
+import moment from 'moment';
+import { Mutation } from 'react-apollo';
+import queries from '../../graphql/queries';
+import Button from '../Button';
+import { UserShortSummaryPropTypes } from '../../propTypes';
+
+const NotificationItem = ({
+  notification,
+  readNotificationCb,
+  deleteNotificationCb,
+}) => {
+  const {
+    _id: notificationId, from, body, createdAt, isRead,
+  } = notification;
+
+  return (
+    <div style={{ background: isRead ? 'white' : 'cyan' }}>
+      <Mutation
+        mutation={queries.READ_NOTIFICATION}
+        variables={{ notificationId }}
+        onCompleted={readNotificationCb}
+      >
+        {readNotification => (
+          <Button text="Mark as Read" onClick={readNotification} />
+        )}
+      </Mutation>
+      <Mutation
+        mutation={queries.DELETE_NOTIFICATION}
+        variables={{ notificationId }}
+        onCompleted={deleteNotificationCb}
+      >
+        {deleteNotification => (
+          <Button text="Delete" onClick={deleteNotification} />
+        )}
+      </Mutation>
+      <span>{`${from.username} ${body}`}</span>
+      <span>{moment(+createdAt).startOf('seconds').fromNow()}</span>
+    </div>
+  );
+};
+
+NotificationItem.propTypes = {
+  notification: shape({
+    _id: string.isRequired,
+    body: string.isRequired,
+    createdAt: string.isRequired,
+    from: UserShortSummaryPropTypes,
+  }).isRequired,
+  readNotificationCb: func.isRequired,
+  deleteNotificationCb: func.isRequired,
+};
+
+export default NotificationItem;
