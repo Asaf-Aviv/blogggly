257bfa37cd034bcdb6fd5ef4604c9fd382415647
Asaf Aviv <asafaviv89@gmail.com>
2019-06-02 18:38:35 +0300

    added disable prop

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { oneOf, func, string } from 'prop-types';
-
+import {
+  oneOf, func, string, bool,
+} from 'prop-types';
 
 const Button = ({
-  classes, type, onClick, text,
+  classes, type, onClick, text, disabled,
 }) => (
   <button
     className={classes}
     type={type}
     onClick={onClick}
+    disabled={disabled}
   >
     {text}
   </button>
@@ -19,12 +21,14 @@ Button.propTypes = {
   onClick: func.isRequired,
   classes: string,
   text: string,
+  disabled: bool,
 };
 
 Button.defaultProps = {
   type: 'button',
   classes: '',
   text: '',
+  disabled: false,
 };
 
 export default Button;
