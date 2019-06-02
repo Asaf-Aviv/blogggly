fe1790b2a53a9c9376dddba507f6391fc1da1c0b
Asaf Aviv <asafaviv89@gmail.com>
2019-06-02 18:39:45 +0300

    update children to display empty sentence

--- client/src/components/NotificationsContainer/NotificationsContainer.js
+++ client/src/components/NotificationsContainer/NotificationsContainer.js
@@ -8,18 +8,18 @@ import './NotificationsContainer.sass';
 
 const NotificationsContainer = ({ classes, children }) => (
   <div className={`notifications__container ${classes}`} onClick={e => e.stopPropagation()}>
-    {children || <EmptySentence />}
+    {children}
+    {children.length <= 2 && !children.every(Boolean) && <EmptySentence />}
   </div>
 );
 
 NotificationsContainer.propTypes = {
   classes: string,
-  children: oneOfType([node, arrayOf(node)]),
+  children: oneOfType([node, arrayOf(node)]).isRequired,
 };
 
 NotificationsContainer.defaultProps = {
   classes: '',
-  children: null,
 };
 
 export default NotificationsContainer;
