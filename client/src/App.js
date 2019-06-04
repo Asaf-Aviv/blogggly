a888273546851dd9b6a536b6a6d9ca6f73df29c6
Asaf Aviv <asafaviv89@gmail.com>
2019-06-04 19:24:09 +0300

    refactor to custom hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import React, {
-  useState, useEffect, useLayoutEffect, useRef,
-} from 'react';
+import React, { useRef } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Alert from 'react-s-alert';
 import { hot } from 'react-hot-loader/root';
 import { useImmer } from 'use-immer';
-import apolloClient, { wsClient } from './ApolloClient';
 import NavBar from './components/NavBar';
 import Users from './components/Users';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import { UserContext, MemberFormsContext } from './context';
-import queries from './graphql/queries';
 import PostEditor from './components/PostEditor';
 import Posts from './components/Posts';
 import Post from './components/Post';
 import Inbox from './components/Inbox/Inbox';
 import UserProfile from './components/UserProfile';
 import CurrentUserProfile from './components/CurrentUserProfile';
-import subscribeToCurrentUserUpdates from './graphql/helpers/subscribeToCurrentUserUpdates';
+import useDarkMode from './hooks/useDarkMode';
+import useToken from './hooks/useToken';
+import useMemberForms from './hooks/useMemberForms';
+import useUserSubscriptions from './hooks/useUserSubsciptions';
 
 import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/jelly.css';
@@ -26,89 +25,22 @@ import './App.sass';
 
 const App = () => {
   const [loggedUser, setLoggedUser] = useImmer(null);
-  const [token, setToken] = useState(null);
+  const [isDarkMode, toggleDarkMode] = useDarkMode();
+  const memberForms = useMemberForms(Boolean(loggedUser));
+  const { setToken, logout } = useToken(Boolean(loggedUser), setLoggedUser);
 
-  const [showMemberForms, setShowMemberForms] = useState(false);
-  const [showLogin, setShowLogin] = useState(true);
-
-  const currentUserSubscriptionRef = useRef();
-
-  const toggleForms = () => {
-    setShowLogin(!showLogin);
-  };
-
-  useEffect(() => {
-    if (loggedUser && !currentUserSubscriptionRef.current) {
-      currentUserSubscriptionRef.current = subscribeToCurrentUserUpdates(
-        setLoggedUser,
-      );
-    }
-    if (!loggedUser && currentUserSubscriptionRef.current) {
-      currentUserSubscriptionRef.current.forEach((subscription) => {
-        subscription.unsubscribe();
-      });
-      currentUserSubscriptionRef.current = null;
-      wsClient.close(false, false);
-    }
-  }, [loggedUser, setLoggedUser]);
-
-  useEffect(() => {
-    if (loggedUser) setShowMemberForms(false);
-  }, [loggedUser, showMemberForms]);
-
-  useLayoutEffect(() => {
-    const cachedToken = localStorage.getItem('token');
-
-    const relog = async () => {
-      try {
-        const { data: { relog: relogResult } } = await apolloClient.mutate({
-          mutation: queries.RELOG,
-        });
-        console.log(relogResult);
-        setLoggedUser(() => relogResult);
-      } catch (error) {
-        setToken(null);
-        localStorage.removeItem('token');
-      }
-    };
-
-    if (cachedToken) relog();
-  }, [setLoggedUser]);
-
-  useEffect(() => {
-    if (token) {
-      localStorage.setItem('token', token);
-      wsClient.close(false, false);
-    }
-  }, [token]);
-
-  const logout = () => {
-    localStorage.removeItem('token');
-    setLoggedUser(() => null);
-    setToken(null);
-  };
+  const subscriptionRef = useRef();
+  useUserSubscriptions(subscriptionRef, loggedUser, setLoggedUser);
 
   return (
     <>
-      <UserContext.Provider
-        value={{
-          loggedUser,
-          setLoggedUser,
-          setToken,
-          isLogged: !!loggedUser,
-        }}
+      <UserContext.Provider value={{
+        loggedUser, setLoggedUser, setToken, isLogged: Boolean(loggedUser),
+      }}
       >
-        <MemberFormsContext.Provider
-          value={{
-            showMemberForms,
-            setShowMemberForms,
-            showLogin,
-            setShowLogin,
-            toggleForms,
-          }}
-        >
-          <div className="App">
-            <NavBar logout={logout} />
+        <MemberFormsContext.Provider value={memberForms}>
+          <div className={`App ${isDarkMode ? 'dark-theme' : ''}`}>
+            <NavBar logout={logout} isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
             <Switch>
               <Route path="/" exact component={Home} />
               <Route path="/posts/tag/:tag" exact component={Posts} />
