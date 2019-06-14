0fbb8eb2596ef43857a1d2671e84402b8b01c42a
Asaf Aviv <asafaviv89@gmail.com>
2019-06-14 18:51:53 +0300

    DarkModeContext

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -2,3 +2,4 @@ import { createContext } from 'react';
 
 export const UserContext = createContext();
 export const MemberFormsContext = createContext();
+export const DarkModeContext = createContext();
