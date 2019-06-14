7935bf630b40f0890b271f49fcf454c05b214443
Asaf Aviv <asafaviv89@gmail.com>
2019-06-14 18:53:40 +0300

    responsive includes

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react';
-import { func, bool } from 'prop-types';
-import { MemberFormsContext, UserContext } from '../../context';
+import React, { useContext, useState } from 'react';
+import { func, number } from 'prop-types';
+import { MemberFormsContext, UserContext, DarkModeContext } from '../../context';
 import Container from '../Container';
 import MemberForms from '../MemberForms';
 import NavMenu from '../NavMenu';
@@ -8,33 +8,43 @@ import NavBarUserPanel from '../NavBarUserPanel';
 import Button from '../Button';
 import Toggler from '../Toggler';
 import NavSearchBar from '../NavSearchBar';
+import { ReactComponent as MenuIcon } from '../../assets/menu.svg';
 
 import './NavBar.sass';
 
-const NavBar = ({ logout, isDarkMode, toggleDarkMode }) => {
-  const { isLogged } = useContext(UserContext);
+const NavBar = ({ logout, windowWidth }) => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
+  const { isLogged } = useContext(UserContext);
   const { showMemberForms, setShowMemberForms, setShowLogin } = useContext(MemberFormsContext);
+  const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext);
+
+  const setNavState = nextState => () => setIsNavOpen(nextState || !isNavOpen);
 
   return (
     <header className="navbar">
       <Container>
         <nav className="nav">
-          <NavMenu />
+          <NavMenu closeNav={setNavState(false)} isNavOpen={isNavOpen} />
+          <div className="nav__section hamburger__container">
+            <MenuIcon className="hamburger" onClick={setNavState()} />
+          </div>
           <NavSearchBar />
-          <Toggler checked={isDarkMode} onChange={toggleDarkMode} />
-          {isLogged ? (
-            <NavBarUserPanel logout={logout} />
-          ) : (
-            <Button
-              classes="login-btn btn btn--primary"
-              text="Log In"
-              onClick={() => {
-                setShowLogin(true);
-                setShowMemberForms(true);
-              }}
-            />
-          )}
+          <div className="nav__section">
+            {windowWidth >= 600 && <Toggler checked={isDarkMode} onChange={toggleDarkMode} />}
+            {isLogged ? (
+              <NavBarUserPanel windowWidth={windowWidth} logout={logout} />
+            ) : (
+              <Button
+                classes="login-btn btn btn--primary"
+                text="Log In"
+                onClick={() => {
+                  setShowLogin(true);
+                  setShowMemberForms(true);
+                }}
+              />
+            )}
+          </div>
           {showMemberForms && !isLogged && <MemberForms />}
         </nav>
       </Container>
@@ -44,8 +54,7 @@ const NavBar = ({ logout, isDarkMode, toggleDarkMode }) => {
 
 NavBar.propTypes = {
   logout: func.isRequired,
-  isDarkMode: bool.isRequired,
-  toggleDarkMode: func.isRequired,
+  windowWidth: number.isRequired,
 };
 
 export default NavBar;
