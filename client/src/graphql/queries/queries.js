16bd9dd1fe1bca37cf86e9d4ab04679055f31025
Asaf Aviv <asafaviv89@gmail.com>
2019-06-14 18:51:37 +0300

    userAuthQueries

--- client/src/graphql/queries/queries.js
+++ client/src/graphql/queries/queries.js
@@ -1,9 +1,11 @@
 import * as userQueries from './post';
+import * as userAuthQueries from './userAuth';
 import * as postQueries from './user';
 import * as commentQueries from './comment';
 
 export default {
   ...userQueries,
+  ...userAuthQueries,
   ...postQueries,
   ...commentQueries,
 };
