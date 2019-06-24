0df91a940904b8f85e2f84cc67ab1153bea5556d
Asaf Aviv <asafaviv89@gmail.com>
2019-06-24 20:38:34 +0300

    add mobile class

--- client/src/components/InboxSideBar/InboxSideBar.js
+++ client/src/components/InboxSideBar/InboxSideBar.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { arrayOf, node, func } from 'prop-types';
+import useWindowWidth from '../../hooks/useWindowWidth';
 
 import './InboxSideBar.sass';
 
-const InboxSideBar = ({ children, setCategory }) => (
-  <nav className="inbox__sidebar">
-    <ul className="inbox__sidebar-menu" onClick={setCategory}>
-      {children}
-    </ul>
-  </nav>
-);
+const InboxSideBar = ({ children, setCategory }) => {
+  const isMobileClass = useWindowWidth() <= 786 && 'inbox__sidebar--mobile';
+
+  return (
+    <nav className={`inbox__sidebar ${isMobileClass}`}>
+      <ul className="inbox__sidebar-menu" onClick={setCategory}>
+        {children}
+      </ul>
+    </nav>
+  );
+};
 
 InboxSideBar.propTypes = {
   children: arrayOf(node).isRequired,
