375f1cfad836dfe39942fb1a0284045b31dba420
Asaf Aviv <asafaviv89@gmail.com>
2019-06-26 22:24:17 +0300

    added google analytics

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
+import ReactGA from 'react-ga';
+import createHistory from 'history/createBrowserHistory';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import apolloClient from './ApolloClient';
 
+ReactGA.initialize('UA-142877210-1');
+
+const history = createHistory();
+
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
+
 const rootEl = document.getElementById('root');
 
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <ApolloProvider client={apolloClient}>
       <App />
     </ApolloProvider>
