b77357cf902f9ee6e2ed2638ae756144766fb799
Asaf Aviv <asafaviv89@gmail.com>
2019-06-26 22:23:49 +0300

    extract Routes to a new file

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
 import React, { useRef } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
 import Alert from 'react-s-alert';
 import { hot } from 'react-hot-loader/root';
 import { useImmer } from 'use-immer';
 import NavBar from './components/NavBar';
-import Users from './components/Users';
-import Home from './components/Home';
 import Footer from './components/Footer';
 import { UserContext, MemberFormsContext, DarkModeContext } from './context';
-import PostEditor from './components/PostEditor';
-import Posts from './components/Posts';
-import Post from './components/Post';
-import PostsByTag from './components/PostsByTag';
-import Inbox from './components/Inbox';
-import Forgot from './components/Forgot';
-import ResetPassword from './components/ResetPassword';
-import Chat from './components/Chat';
-import UserProfile from './components/UserProfile';
-import CurrentUserProfile from './components/CurrentUserProfile';
+// import Chat from './components/Chat';
+import Routes from './components/Routes';
 import useDarkMode from './hooks/useDarkMode';
 import useToken from './hooks/useToken';
 import useMemberForms from './hooks/useMemberForms';
@@ -49,21 +38,8 @@ const App = () => {
           <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
             <div className="App">
               <NavBar logout={logout} windowWidth={windowWidth} />
-              <Chat />
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/posts" component={Posts} />
-                <Route path="/posts/tag/:tag" component={PostsByTag} />
-                <Route path="/post/:postId" component={Post} />
-                <Route path="/user/:username" component={UserProfile} />
-                <Route path="/users" component={Users} />
-                <Route path="/profile" component={CurrentUserProfile} />
-                <Route path="/inbox" component={Inbox} />
-                <Route path="/create" component={PostEditor} />
-                <Route path="/forgot" component={Forgot} />
-                <Route path="/reset/:resetToken" component={ResetPassword} />
-                <Route render={() => <Redirect to="/" />} />
-              </Switch>
+              {/* <Chat /> */}
+              <Routes />
               <Footer />
             </div>
           </DarkModeContext.Provider>
